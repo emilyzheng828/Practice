8d0c843865563cabefe454619028e615cb6b2f46
Emily_rrz <emily.zheng828@gmail.com>
2015-11-18 13:51:45 +0000

    omg, it is working so happy

--- most_frequent_letter.rb
+++ most_frequent_letter.rb
@@ -3,10 +3,9 @@ puts "Give me a letter"
 letters = gets.chomp.split(//)
 n = letters.size
 hash = {}
-n.times do |i,num|
 letters.each {|i, num| hash[i] = letters.count(i)}
-puts hash
-end
+num = hash.values.max
+hash.key(num)
 end
 
 
