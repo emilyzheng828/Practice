1c2f526c4338dfac4a54ee013b322be4166c3bb3
Emily_rrz <emily.zheng828@gmail.com>
2015-11-18 13:53:49 +0000

    small changes, remove num

--- most_frequent_letter.rb
+++ most_frequent_letter.rb
@@ -1,11 +1,9 @@
 def letter_check
 puts "Give me a letter"
 letters = gets.chomp.split(//)
-n = letters.size
 hash = {}
 letters.each {|i, num| hash[i] = letters.count(i)}
-num = hash.values.max
-hash.key(num)
+hash.key(hash.values.max)
 end
 
 
